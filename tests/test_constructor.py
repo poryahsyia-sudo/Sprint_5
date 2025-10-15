72ad5d8db062bd65987480e8779e5bf9e1411d5a
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-15 19:22:11 +0500

    исправила ошибку в BUNS_TAB

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -8,11 +8,9 @@ from data.urls import BASE_URL
 def test_fillings_tab(driver):
     driver.get(BASE_URL)
     driver.find_element(*ConstructorPageLocators.FILLINGS_TAB).click()
-
     WebDriverWait(driver, 5).until(
         EC.visibility_of_element_located(IngredientsLocators.FILLING_MEAT)
     )
-
     fillings_tab = driver.find_element(*ConstructorPageLocators.FILLINGS_TAB)
     assert "tab_tab_type_current" in fillings_tab.get_attribute("class")
 
@@ -20,22 +18,20 @@ def test_fillings_tab(driver):
 def test_sauces_tab(driver):
     driver.get(BASE_URL)
     driver.find_element(*ConstructorPageLocators.SAUCES_TAB).click()
-
     WebDriverWait(driver, 5).until(
         EC.visibility_of_element_located(IngredientsLocators.SAUCE_SPICY_X)
     )
-
     sauces_tab = driver.find_element(*ConstructorPageLocators.SAUCES_TAB)
     assert "tab_tab_type_current" in sauces_tab.get_attribute("class")
 
 
 def test_buns_tab(driver):
     driver.get(BASE_URL)
-    driver.find_element(*ConstructorPageLocators.BUNS_TAB).click()
-
+    WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(ConstructorPageLocators.BUNS_TAB)
+    ).click()
     WebDriverWait(driver, 5).until(
         EC.visibility_of_element_located(IngredientsLocators.BUN_R2D3)
     )
-
     buns_tab = driver.find_element(*ConstructorPageLocators.BUNS_TAB)
     assert "tab_tab_type_current" in buns_tab.get_attribute("class")
