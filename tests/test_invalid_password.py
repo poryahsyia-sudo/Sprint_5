0f0b60364d29803df01de817365f14a0773ce16e
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-15 20:21:52 +0500

    .

--- tests/test_invalid_password.py
+++ tests/test_invalid_password.py
@@ -1,15 +1,18 @@
-import time
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.login_page import LoginPage
 from pages.locators import LoginPageLocators
 from data.urls import BASE_URL
 
+
 @pytest.mark.usefixtures("driver")
 class TestInvalidPassword:
     def test_password_failure(self, driver, invalid_password):
-        driver.get(BASE_URL)
-        login_page.open(BASE_URL)
+        login_page = LoginPage(driver)
+        login_page.open(BASE_URL + "login")
         login_page.login(invalid_password["email"], invalid_password["password"])
-        time.sleep(2)
-        # Ожидаем, что кнопка "Войти" всё ещё отображается (вход не выполнен)
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(LoginPageLocators.LOGIN_BUTTON)
+        )
         assert driver.find_element(*LoginPageLocators.LOGIN_BUTTON).is_displayed()
--- tests/test_registration_and_login.py
+++ tests/test_registration_and_login.py
@@ -1,5 +1,6 @@
-import time
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from utils.generator import generate_email, generate_password
 from pages.locators import (
     MainPageLocators,
@@ -7,104 +8,77 @@ from pages.locators import (
     LoginPageLocators,
     PasswordRecoveryPageLocators,
 )
+from data.urls import BASE_URL
+
 
 def test_successful_registration(driver):
-    driver.get("https://stellarburgers.education-services.ru/")
-
-    # Переход на страницу регистрации
+    driver.get(BASE_URL)
     driver.find_element(*MainPageLocators.LOGIN_ACCOUNT_BUTTON).click()
     driver.find_element("link text", "Зарегистрироваться").click()
-
-    # Заполнение формы
     email = generate_email()
     password = generate_password()
     driver.find_element(*RegistrationPageLocators.NAME_FIELD).send_keys("Test User")
     driver.find_element(*RegistrationPageLocators.EMAIL_FIELD).send_keys(email)
     driver.find_element(*RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
-
-    # Отправка формы
     driver.find_element(*RegistrationPageLocators.REGISTER_BUTTON).click()
-    time.sleep(3)
-
-    # Вход с только что зарегистрированным пользователем
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(LoginPageLocators.LOGIN_BUTTON)
+    )
     driver.find_element(*LoginPageLocators.EMAIL_FIELD).send_keys(email)
     driver.find_element(*LoginPageLocators.PASSWORD_FIELD).send_keys(password)
     driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-    time.sleep(3)
-
-    # Проверка успешного входа по кнопке "Оформить заказ"
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(LoginPageLocators.ORDER_BUTTON)
+    )
     assert driver.find_element(*LoginPageLocators.ORDER_BUTTON).is_displayed()
-    
+
 
 def test_login_from_main_page(driver, valid_user):
-    driver.get("https://stellarburgers.education-services.ru/")
-    time.sleep(1)
-
-    # Нажимаем кнопку "Войти в аккаунт"
+    driver.get(BASE_URL)
     driver.find_element(*MainPageLocators.LOGIN_ACCOUNT_BUTTON).click()
-    time.sleep(1)
-
-    # Вводим Email и пароль
     driver.find_element(*LoginPageLocators.EMAIL_FIELD).send_keys(valid_user["email"])
     driver.find_element(*LoginPageLocators.PASSWORD_FIELD).send_keys(valid_user["password"])
-
-    # Нажимаем кнопку "Войти"
     driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-    time.sleep(2)
-
-    # Проверка успешного входа через наличие кнопки "Оформить заказ"
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(LoginPageLocators.ORDER_BUTTON)
+    )
     assert driver.find_element(*LoginPageLocators.ORDER_BUTTON).is_displayed()
+
 
 def test_login_via_personal_account(driver, valid_user):
-    driver.get("https://stellarburgers.education-services.ru/")
-    time.sleep(1)
-
-    # Нажимаем кнопку "Личный кабинет"
+    driver.get(BASE_URL)
     driver.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
-    time.sleep(1)
-
-    # Проверяем, что форма Email/Пароль отображается
-    assert driver.find_element(*LoginPageLocators.EMAIL_FIELD).is_displayed()
-    assert driver.find_element(*LoginPageLocators.PASSWORD_FIELD).is_displayed()
-    assert driver.find_element(*LoginPageLocators.LOGIN_BUTTON).is_displayed()
-
-    # Вводим данные и входим
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(LoginPageLocators.EMAIL_FIELD)
+    )
     driver.find_element(*LoginPageLocators.EMAIL_FIELD).send_keys(valid_user["email"])
     driver.find_element(*LoginPageLocators.PASSWORD_FIELD).send_keys(valid_user["password"])
     driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-    time.sleep(2)
-
-    # Проверяем успешный вход
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(LoginPageLocators.ORDER_BUTTON)
+    )
     assert driver.find_element(*LoginPageLocators.ORDER_BUTTON).is_displayed()
+
 
 def test_login_from_registration_page(driver, valid_user):
-    driver.get("https://stellarburgers.education-services.ru/register")
-    time.sleep(1)
-
-    # Нажимаем кнопку "Войти" на форме регистрации
+    driver.get(BASE_URL + "register")
     driver.find_element(*RegistrationPageLocators.LOGIN_BUTTON_ON_REG_FORM).click()
-    time.sleep(1)
-
-    # Вводим email и пароль валидного пользователя и входим
     driver.find_element(*LoginPageLocators.EMAIL_FIELD).send_keys(valid_user["email"])
     driver.find_element(*LoginPageLocators.PASSWORD_FIELD).send_keys(valid_user["password"])
     driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-    time.sleep(2)
-
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(LoginPageLocators.ORDER_BUTTON)
+    )
     assert driver.find_element(*LoginPageLocators.ORDER_BUTTON).is_displayed()
 
+
 def test_login_from_password_recovery_page(driver, valid_user):
-    driver.get("https://stellarburgers.education-services.ru/forgot-password")
-    time.sleep(1)
-
-    # Нажимаем кнопку "Войти" на форме восстановления
+    driver.get(BASE_URL + "forgot-password")
     driver.find_element(*PasswordRecoveryPageLocators.LOGIN_BUTTON_ON_RECOVER_FORM).click()
-    time.sleep(1)
-
-    # Вводим данные и входим
     driver.find_element(*LoginPageLocators.EMAIL_FIELD).send_keys(valid_user["email"])
     driver.find_element(*LoginPageLocators.PASSWORD_FIELD).send_keys(valid_user["password"])
     driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-    time.sleep(2)
-
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(LoginPageLocators.ORDER_BUTTON)
+    )
     assert driver.find_element(*LoginPageLocators.ORDER_BUTTON).is_displayed()
