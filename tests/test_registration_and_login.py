7ffbdc4db48043b5552e0c765ea2f0708847b933
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-17 19:22:42 +0500

    .

--- tests/test_registration_and_login.py
+++ tests/test_registration_and_login.py
@@ -1,33 +1,49 @@
 import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from utils.generator import generate_email, generate_password
-from pages.locators import (
-    MainPageLocators,
-    RegistrationPageLocators,
-    LoginPageLocators,
-    PasswordRecoveryPageLocators,
-)
+from selenium.webdriver.common.by import By
 from data.urls import BASE_URL
+from pages.registration_page import RegistrationPage
+from pages.login_page import LoginPage
+from pages.profile_page import ProfilePage
 
 
-def test_successful_registration(driver):
-    driver.get(BASE_URL)
-    driver.find_element(*MainPageLocators.LOGIN_ACCOUNT_BUTTON).click()
-    driver.find_element("link text", "Зарегистрироваться").click()
-    email = generate_email()
-    password = generate_password()
-    driver.find_element(*RegistrationPageLocators.NAME_FIELD).send_keys("Test User")
-    driver.find_element(*RegistrationPageLocators.EMAIL_FIELD).send_keys(email)
-    driver.find_element(*RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
-    driver.find_element(*RegistrationPageLocators.REGISTER_BUTTON).click()
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(LoginPageLocators.LOGIN_BUTTON)
-    )
-    driver.find_element(*LoginPageLocators.EMAIL_FIELD).send_keys(email)
-    driver.find_element(*LoginPageLocators.PASSWORD_FIELD).send_keys(password)
-    driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(LoginPageLocators.ORDER_BUTTON)
-    )
-    assert driver.find_element(*LoginPageLocators.ORDER_BUTTON).is_displayed()
+@pytest.mark.usefixtures("driver")
+class TestRegistrationAndLogin:
+
+    def test_successful_registration(self, driver, valid_user):
+        """Проверка успешной регистрации"""
+        page = RegistrationPage(driver)
+        page.open(BASE_URL)
+        page.register(valid_user["name"], valid_user["email"], valid_user["password"])
+        assert driver.find_element(By.XPATH, "//p[contains(text(), 'Личный кабинет')]")
+
+    def test_login_from_main_page(self, driver, valid_user):
+        """Проверка входа через кнопку 'Войти в аккаунт' на главной странице"""
+        driver.get(BASE_URL)
+        driver.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click()
+        login_page = LoginPage(driver)
+        login_page.login(valid_user["email"], valid_user["password"])
+        assert driver.find_element(By.XPATH, "//p[contains(text(), 'Личный кабинет')]")
+
+    def test_login_via_personal_account(self, driver, valid_user):
+        """Проверка входа через кнопку 'Личный кабинет'"""
+        driver.get(BASE_URL)
+        driver.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click()
+        login_page = LoginPage(driver)
+        login_page.login(valid_user["email"], valid_user["password"])
+        assert driver.find_element(By.XPATH, "//p[contains(text(), 'Личный кабинет')]")
+
+    def test_login_from_registration_page(self, driver, valid_user):
+        """Проверка входа через ссылку 'Войти' со страницы регистрации"""
+        driver.get(BASE_URL + "register")
+        driver.find_element(By.XPATH, "//a[text()='Войти']").click()
+        login_page = LoginPage(driver)
+        login_page.login(valid_user["email"], valid_user["password"])
+        assert driver.find_element(By.XPATH, "//p[contains(text(), 'Личный кабинет')]")
+
+    def test_login_from_password_recovery_page(self, driver, valid_user):
+        """Проверка входа через ссылку 'Войти' со страницы восстановления пароля"""
+        driver.get(BASE_URL + "forgot-password")
+        driver.find_element(By.XPATH, "//a[text()='Войти']").click()
+        login_page = LoginPage(driver)
+        login_page.login(valid_user["email"], valid_user["password"])
+        assert driver.find_element(By.XPATH, "//p[contains(text(), 'Личный кабинет')]")
